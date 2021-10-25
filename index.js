4a4a22243052613c9a1854366b9b8fe5614c1b3c
Lavaruz <muzakiassami@gmail.com>
2021-10-25 20:21:36 +0700

    final

--- index.js
+++ index.js
@@ -4,46 +4,59 @@ const speakerImgMute = document.getElementById('speakerImgMute')
 const speakerImgUp = document.getElementById('speakerImgUp')
 const audio = document.querySelector('.audio')
 
-window.addEventListener('load', function(){
-    loadingPage.classList.add('scale-down-center')
+window.addEventListener('load', function () {
+  loadingPage.classList.add('scale-down-center')
 })
-speakerImgMute.addEventListener('click', function(){
-    audio.play()
-    speakerImgMute.style.display = "none"
-    speakerImgUp.style.display = "block"
+speakerImgMute.addEventListener('click', function () {
+  audio.play()
+  speakerImgMute.style.display = "none"
+  speakerImgUp.style.display = "block"
 })
-speakerImgUp.addEventListener('click', function(){
-    audio.pause()
-    speakerImgMute.style.display = "block"
-    speakerImgUp.style.display = "none"
+speakerImgUp.addEventListener('click', function () {
+  audio.pause()
+  speakerImgMute.style.display = "block"
+  speakerImgUp.style.display = "none"
 })
 
 // Clicker
 
 var coll = document.querySelectorAll('.card');
 var i;
-var x = window.matchMedia('(max-width:900px)')
+var x = window.matchMedia('(max-width:970px)')
 const werehouse = document.querySelector('.portfolio-heading h1')
 
-    if (x.matches) {
-        werehouse.textContent = "- Werehouse -"
-        for (i = 0; i < coll.length; i++) {
-            coll[i].addEventListener("click", function() {
-            var content = this.lastElementChild.lastElementChild
-            if (content.style.display === "block") {
-              content.style.display = "none";
-            } else {
-              content.style.display = "block";
-            }
-            if (content.style.maxHeight){
-                content.style.maxHeight = null;
-              } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-              }
-          });
+if (x.matches) {
+  werehouse.textContent = "- Werehouse -"
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      var content = this.lastElementChild.lastElementChild
+      this.classList.toggle("active");
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+} else {
+  werehouse.textContent = "Werehouse"
+}
+
+window.addEventListener('resize', function(){
+  if (x.matches) {
+    werehouse.textContent = "- Werehouse -"
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function () {
+        var content = this.lastElementChild.lastElementChild
+        this.classList.toggle("active");
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
         }
-    }else{
-        werehouse.textContent = "Werehouse"
+      });
     }
-
-
+  } else {
+    werehouse.textContent = "Werehouse"
+  }
+})
